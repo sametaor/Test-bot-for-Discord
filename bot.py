db2b3906c95f17421c530c8d719545ab03a50770
sametaor <sambit.acharya1@outlook.com>
2021-05-27 13:09:16 +0530

    Made some changes to help and added level system

--- bot.py
+++ bot.py
@@ -3,33 +3,57 @@ import asyncio
 import json
 import requests
 import random
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discordtoken import discord_token, api_key
 from texttoowo import text_to_owo
 from redditmeme import reddit
+from itertools import cycle
 from tictactoe import winningConditions, player1, player2, turn, gameOver, board
 from weatherassets import *
 from prsaw import RandomStuff
 
 
 testbot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+testbot.remove_command("help")
 
-startup_extensions = ["Cogsforbot.help", "Cogsforbot.chess", "Cogsforbot.snakegameassets", "Cogsforbot.UrbanDictionary"]
+startup_extensions = ["Cogsforbot.Coghelp", "Cogsforbot.chess", "Cogsforbot.snakegameassets", "Cogsforbot.UrbanDictionary"]
 
 filtered_words = ["fuck", "bullshit"]
 
 rs = RandomStuff(async_mode=True)
 
+status = cycle([
+    'discord.py',
+    '$help',
+    'Snake',
+    'Tictactoe',
+    'Chess',
+    '[insert your favourite game here]'
+])
+
+@tasks.loop(seconds=5)
+async def status_swap():
+    await testbot.change_presence(activity=discord.Game(next(status)))
+
+
 @testbot.event
 async def on_ready():
     print("Test bot ready to go")
-    await testbot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='$help'))
+    status_swap.start()
 
 
 @testbot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='welcome')
     await channel.send(f"Hello there {member.mention}! A warm welcome to you for joining {member.guild.name}!")
+
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f, indent=4)
 
 
 @testbot.event
@@ -50,7 +74,6 @@ async def on_message(msg):
         response = await rs.get_ai_response(msg.content)
         await msg.reply(response)
     
-    await testbot.process_commands(msg)
     if msg.author != testbot.user and msg.content.startswith('$weather'):
         if len((msg.content.replace('$weather ', ''))) >= 1:
             location = msg.content.replace('$weather ', '')
@@ -60,7 +83,19 @@ async def on_message(msg):
                 await msg.channel.send(embed=weathermsg(data, location))
             except KeyError:
                 await msg.channel.send(embed=error_message(location))
+    
+    if msg.author.bot == False:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        
+        await update_data(users, msg.author)
+        await add_experience(users, msg.author, 5)
+        await level_up(users, msg.author, msg)
 
+        with open('users.json', 'w') as f:
+            json.dump(users, f, indent=4)
+    
+    await testbot.process_commands(msg)
 
 @testbot.event
 async def on_command_error(ctx,error):
@@ -78,45 +113,40 @@ async def on_command_error(ctx,error):
 
 @testbot.event
 async def on_raw_reaction_add(payload):
-    rolereactionID = 842021454279475220
+    
+    if payload.member.bot:
+        pass
 
-    if rolereactionID ==payload.message_id:
-        member = payload.member
-        guild = member.guild
+    else:
+        with open('giverole.json') as react_file:
+            data = json.load(react_file)
+            for x in data:
+                if str(x['emoji']) == payload.emoji.name:
+                    role = discord.utils.get(testbot.get_guild(payload.guild_id).roles, id=x['role_id'])
 
-        emoji = payload.emoji.name
-        if emoji == '🍎':
-            role = discord.utils.get(guild.roles, name="Apple")
-        elif emoji == '🥭':
-            role = discord.utils.get(guild.roles, name="Mango")
-        elif emoji == '🍇':
-            role = discord.utils.get(guild.roles, name="Grapes")
-        elif emoji == '🍐':
-            role = discord.utils.get(guild.roles, name="Pear")
-        await member.add_roles(role)
+                    await payload.member.add_roles(role)
+
 
 @testbot.event
 async def on_raw_reaction_remove(payload):
-    rolereactionID = 842021454279475220
+    with open('giverole.json') as react_file:
+        data = json.load(react_file)
+        for x in data:
+            if str(x['emoji']) == payload.emoji.name:
+                role = discord.utils.get(testbot.get_guild(payload.guild_id).roles, id=x['role_id'])
 
-    if rolereactionID ==payload.message_id:
-        guild = await(testbot.fetch_guild(payload.guild_id))
+                await testbot.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
 
-        emoji = payload.emoji.name
-        if emoji == '🍎':
-            role = discord.utils.get(guild.roles, name="Apple")
-        elif emoji == '🥭':
-            role = discord.utils.get(guild.roles, name="Mango")
-        elif emoji == '🍇':
-            role = discord.utils.get(guild.roles, name="Grapes")
-        elif emoji == '🍐':
-            role = discord.utils.get(guild.roles, name="Pear")
-        member = await(guild.fetch_member(payload.user_id))
-        if member is not None:
-            await member.remove_roles(role)
-        else:
-            print("Member not found")
 
+@testbot.group(invoke_without_command=True)
+async def help(ctx):
+    em = discord.Embed(title="May I help you?", description="Use $help <command> for detailed info of a command", colour=ctx.author.colour)
+    em.add_field(name="Moderation", value = "purge, kick, ban, unban, mute, unmute, tempmute, lockdown, unlock, slowmode, giverole, nickset, whois")
+    em.add_field(name="Reddit", value="meme, formuladank, crapmcsuggest, verbose, engrish")
+    em.add_field(name="Fun", value="owofy, guess, rolldice, coinflip, urban, sing")
+    em.add_field(name="Games", value="chess, snek, tictactoe, place, board, end")
+    em.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
+    await ctx.send(embed=em)
 
 @testbot.command()
 @commands.has_permissions(manage_messages=True)
@@ -236,6 +266,7 @@ async def unlock(ctx):
     await ctx.send("Lockdown mode is now removed for " + ctx.channel.mention)
 
 @testbot.command()
+@commands.has_permissions(manage_messages=True)
 async def slowmode(ctx, seconds : int):
     await ctx.channel.edit(slowmode_delay=seconds)
     await ctx.send(f"The slowmode is now set to {seconds} seconds.")
@@ -262,17 +293,26 @@ async def whois(ctx, member : discord.Member):
 
 @testbot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
-async def giverole(ctx):
-    embed = discord.Embed(title="Reaction roles", description="Choose a role from the following set of reaction roles!", colour=discord.Colour.blue())
-    msg = await ctx.send(embed=embed)
-    #Apple
-    await msg.add_reaction('🍎')
-    #Mango
-    await msg.add_reaction('🥭')
-    #Grapes
-    await msg.add_reaction('🍇')
-    #Pear
-    await msg.add_reaction('🍐')
+async def giverole(ctx, emoji,role : discord.Role,*,message):
+    embed = discord.Embed(description=message)
+    rolemsg = await ctx.channel.send(embed=embed)
+    await rolemsg.add_reaction(emoji)
+
+    with open('giverole.json') as json_file:
+        data = json.load(json_file)
+
+        new_react_role = {
+            'role_name' : role.name,
+            'role_id' : role.id,
+            'emoji' : emoji,
+            'message_id' : rolemsg.id
+        }
+
+        data.append(new_react_role)
+    
+    with open('giverole.json','w') as j:
+        json.dump(data,j,indent=4)
+
 
 @testbot.command()
 async def meme(ctx, memes="memes"):
@@ -338,7 +378,7 @@ async def engrish(ctx, ripenglish="engrish"):
     await ctx.send(embed=em)
 
 @testbot.command()
-async def shitmcsuggest(ctx, shitmc="shittymcsuggestions"):
+async def crapmcsuggest(ctx, shitmc="shittymcsuggestions"):
     subreddit = reddit.subreddit(shitmc)
     all_subs = []
 
@@ -556,8 +596,191 @@ async def place_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send("Please make sure to enter an integer.")
 
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
+
+async def add_experience(users, user, exp):
+    users[f'{user.id}']['experience'] += exp
+
+async def level_up(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1/4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has levelled up! LEVEL - {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+
+@testbot.command()
+async def rank(ctx, member : discord.Member):
+    if not member:
+        id = ctx.message.author.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'You are at level {lvl}!')
+    else:
+        id =  member.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'{member} is at level {lvl}!')
+
+@help.command()
+async def purge(ctx):
+    em = discord.Embed(title="purge", description="Clears the last sent msgs in a channel.")
+    em.add_field(name="**Syntax**", value="$purge <number of msgs to be deleted>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def kick(ctx):
+    em = discord.Embed(title="kick", description="Kicks the specified member out of a server.")
+    em.add_field(name="**Syntax**", value="$kick <@member> <reason>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def ban(ctx):
+    em = discord.Embed(title="ban", description="Bans the specified member from a server.")
+    em.add_field(name="**Syntax**", value="$ban <@member> <reason>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def unban(ctx):
+    em = discord.Embed(title="unban", description="Unbans a user from a server.")
+    em.add_field(name="**Syntax**", value="$unban <username> ")
+    await ctx.send(embed=em)
+
+@help.command()
+async def mute(ctx):
+    em = discord.Embed(title="mute", description="Mutes the specified member in a server.")
+    em.add_field(name="**Syntax**", value="$mute <@member> <reason>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def unmute(ctx):
+    em = discord.Embed(title="unmute", description="Unmutes the specified member in a server.")
+    em.add_field(name="**Syntax**", value="$unmute <@member>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def tempmute(ctx):
+    em = discord.Embed(title="tempmute", description="Temporarily mutes the specified member for a specified amount of time in a server.")
+    em.add_field(name="**Syntax**", value="$tempmute <@member> <time> <reason>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def lockdown(ctx):
+    em = discord.Embed(title="lockdown", description="Locks the specified channel to stop members from messaging.")
+    em.add_field(name="**Syntax**", value="$lockdown")
+    await ctx.send(embed=em)
+
+@help.command()
+async def unlock(ctx):
+    em = discord.Embed(title="unlock", description="Unlocks the specified channel to allow members to send messages.")
+    em.add_field(name="**Syntax**", value="$unlock")
+    await ctx.send(embed=em)
+
+@help.command()
+async def slowmode(ctx):
+    em = discord.Embed(title="slowmode", description="Applies a specified amount of slowmode in the specified channel.")
+    em.add_field(name="**Syntax**", value="$slowmode <time in seconds>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def nickset(ctx):
+    em = discord.Embed(title="nickset", description="Lets one set a custom nickname in the server.")
+    em.add_field(name="**Syntax**", value="$nickset <@member> <preferred nickname>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def sing(ctx):
+    em = discord.Embed(title="sing", description="Makes the bot sing a line off of the song Money Game Part2 by Ren.")
+    em.add_field(name="**Syntax**", value="$sing")
+    await ctx.send(embed=em)
+
+@help.command()
+async def whois(ctx):
+    em = discord.Embed(title="whois", description="Lets one view the user info of the specified user.")
+    em.add_field(name="**Syntax**", value="$whois <@member>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def meme(ctx):
+    em = discord.Embed(title="meme", description="Sends contentfrom r/memes.")
+    em.add_field(name="**Syntax**", value="$meme")
+    await ctx.send(embed=em)
+
+@help.command()
+async def formuladank(ctx):
+    em = discord.Embed(title="formuladank", description="Sends content from r/formuladank.")
+    em.add_field(name="**Syntax**", value="$formuladank")
+    await ctx.send(embed=em)
+
+@help.command()
+async def crapmcsuggest(ctx):
+    em = discord.Embed(title="crapmcsuggest", description="Sends content from r/shittymcsuggestions.")
+    em.add_field(name="**Syntax**", value="$crapmcsuggest")
+    await ctx.send(embed=em)
+
+@help.command()
+async def verbose(ctx):
+    em = discord.Embed(title="verbose", description="Sends content from r/IncreasinglyVerbose.")
+    em.add_field(name="**Syntax**", value="$verbose")
+    await ctx.send(embed=em)
+
+@help.command()
+async def engrish(ctx):
+    em = discord.Embed(title="engrish", description="Sends content from r/engrish.")
+    em.add_field(name="**Syntax**", value="$engrish")
+    await ctx.send(embed=em)
+
+@help.command()
+async def owofy(ctx):
+    em = discord.Embed(title="purge", description="Decorates the specified text with cursed OwOs.")
+    em.add_field(name="**Syntax**", value="$owofy <text>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def guess(ctx):
+    em = discord.Embed(title="guess", description="Starts a guessing game with three turns.")
+    em.add_field(name="**Syntax**", value="$guess")
+    await ctx.send(embed=em)
+
+@help.command()
+async def rolldice(ctx):
+    em = discord.Embed(title="rolldice", description="Gives a random face of a dice.")
+    em.add_field(name="**Syntax**", value="$rolldice")
+    await ctx.send(embed=em)
+
+@help.command()
+async def coinflip(ctx):
+    em = discord.Embed(title="coinflip", description="Gives either Heads or Tails face of a coin.")
+    em.add_field(name="**Syntax**", value="$coinflip")
+    await ctx.send(embed=em)
+
+@help.command()
+async def tictactoe(ctx):
+    em = discord.Embed(title="tictactoe", description="Starts a match of tictactoe with another mentioned member.")
+    em.add_field(name="**Syntax**", value="$tictactoe <@member1> <@member2>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def place(ctx):
+    em = discord.Embed(title="place(related to tictactoe)", description="Used to define positions for either a ❌ or a ⭕.")
+    em.add_field(name="**Syntax**", value="$place <position>")
+    await ctx.send(embed=em)
+
+@help.command()
+async def board(ctx):
+    em = discord.Embed(title="board(related to tictactoe)", description="Used to show the positions on the tictactoe board.")
+    em.add_field(name="**Syntax**", value="$board")
+    await ctx.send(embed=em)
+
 if __name__ == "__main__":  # When script is loaded, this will run
-    testbot.remove_command("help")
     for extension in startup_extensions:
         try:
             testbot.load_extension(extension)  # Loads cogs successfully

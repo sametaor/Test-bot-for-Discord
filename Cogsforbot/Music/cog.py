a32132c9df2b2515b62d7bf8b322f52eea89ecbc
sametaor <sambit.acharya1@outlook.com>
2022-03-06 19:37:52 +0530

    added queue and loop functions

--- Cogsforbot/Music/cog.py
+++ Cogsforbot/Music/cog.py
@@ -22,6 +22,18 @@ class Music(commands.Cog):
     async def on_wavelink_node_ready(self, node: wavelink.Node):
         print(f"Node {node.identifier} is ready!")
     
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(player: wavelink.Player, track: wavelink.Track, reason):
+        ctx = player.ctx
+        vc: player = ctx.voice_client
+        
+        if vc.loop:
+            return await vc.play(track)
+        
+        next_song = vc.queue.get()
+        await vc.play(next_song)
+        await ctx.send(f"Now playing: {next_song.title}")
+        
     @commands.group(invoke_without_command=True)
     @commands.guild_only()
     async  def play(self, ctx):
@@ -36,12 +48,18 @@ class Music(commands.Cog):
         else:
             vc: wavelink.Player = ctx.voice_client
         
-        await vc.play(search)
-        conversion = datetime.timedelta(seconds=search.duration)
-        musicduration = str(conversion)
-        musicembed = nextcord.Embed(title="Now Playing", description=f"Song: [{search.title}]({search.uri})\n Artist: `{search.author}`\n Duration: `{musicduration}`")
-        musicembed.set_thumbnail(url=search.thumb)
-        await ctx.send(embed=musicembed)
+        if vc.queue.is_empty and vc.is_playing:
+            await vc.play(search)
+            conversion = datetime.timedelta(seconds=search.duration)
+            musicduration = str(conversion)
+            musicembed = nextcord.Embed(title="Now Playing", description=f"Song: [{search.title}]({search.uri})\n Artist: `{search.author}`\n Duration: `{musicduration}`")
+            musicembed.set_thumbnail(url=search.thumb)
+            await ctx.send(embed=musicembed)
+        else:
+            vc.queue.put_wait(search)
+            await ctx.send(f"Added `{search.title}` to the queue :)")
+        vc.ctx = ctx
+        setattr(vc, "loop", False)
 
     @commands.command()
     async def pause(self, ctx: commands.Context):
@@ -102,6 +120,46 @@ class Music(commands.Cog):
         
         await vc.set_volume(int(volumevalue))
         await ctx.send(f"Changing Volume to `{volumevalue}`...")
+    
+    @commands.command()
+    async def loop(self, ctx: commands.Context):
+        if not ctx.voice_client:
+            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        elif not getattr(ctx.author.voice, "channel", None):
+            return await ctx.send("Please join a Voice Channel first, and then type '&play track <query>'")
+        else:
+            vc: wavelink.Player = ctx.voice_client
+        
+        try:
+            vc.loop ^= True
+        except:
+            setattr(vc, "loop", False)
+        
+        if vc.loop:
+            return await ctx.send("Loop is now enabled!")
+        else:
+            return await ctx.send("Loop is now disabled!")
+    
+    @commands.command()
+    async def queue(self, ctx: commands.Context):
+        if not ctx.voice_client:
+            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        elif not getattr(ctx.author.voice, "channel", None):
+            return await ctx.send("Please join a Voice Channel first, and then type '&play track <query>'")
+        else:
+            vc: wavelink.Player = ctx.voice_client
+        
+        if vc.queue.is_empty:
+            return await ctx.send("Queue is empty!")
+        
+        em = nextcord.Embed(title="Queue")
+        queue = vc.queue.copy()
+        song_count = 0
+        for song in queue:
+            song_count += 1
+            em.add_field(name=f"Song Num {song_count}", value=f"`{song.title}`")
+        
+        return await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(Music(bot))

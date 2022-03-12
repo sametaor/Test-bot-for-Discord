5d37a339cca493e31582a26f4f4cd41851cdb0a7
sametaor <sambit.acharya1@outlook.com>
2022-03-12 19:59:19 +0530

    Added a new Activity game

---
+++ Cogsforbot/Activities/cog.py
@@ -0,0 +1,31 @@
+import nextcord
+from nextcord.ext import commands, activities
+
+class MakeLinkBtn(nextcord.ui.View):
+    def __init__(self, link:str):
+        super().__init__()
+        self.add_item(nextcord.ui.Button(label="Join Game!", url=f"{link}"))
+
+class Activities(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.group(invoke_without_command=True)
+    async def play(ctx):
+        return
+    
+    @play.command()
+    async def sketch(ctx, channel: nextcord.VoiceChannel = None):
+        if channel == None:
+            return await ctx.send("Please specify a channel to join/create a game")
+        try:
+            invite_link =  await channel.create_activity_invite(activities.Activity.sketch)
+        except nextcord.HTTPException:
+            return await ctx.send("Please mention a voice channel to join/create a game")
+        sketchembed = nextcord.Embed(title="Sketch game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+        sketchembed.add_field(name="How to Play?", value="If you have played games like skribble.io and gartic phone, this is basically it, where one draws something, and the rest try to guess what it's supposed to be!")
+        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+
+def setup(bot):
+    bot.add_cog(Activities(bot))

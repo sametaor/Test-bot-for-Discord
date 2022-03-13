598a19d5f877004e19469861fd355b0d0049f6b3
sametaor <sambit.acharya1@outlook.com>
2022-03-13 13:56:31 +0530

    Added image manipulation command

---
+++ Cogsforbot/ImageManipulation/cog.py
@@ -0,0 +1,27 @@
+import nextcord
+from nextcord.ext import commands
+from PIL import Image
+from io import BytesIO
+
+class MemeGen(commands.Cog, name="Meme Generation"):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def wanted(self, ctx, user: nextcord.Member = None):
+        if user == None:
+            user = ctx.author
+        
+        wanted = Image.open("Cogsforbot/ImageManipulation/Wanted.jpg")
+        asset = user.avatar.url.with_size(128)
+        
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        
+        pfp.resize((283,283))
+        wanted.paste(pfp, (96,230))
+        wanted.save("profile.jpg")
+        await ctx.send(file = nextcord.File("profile.jpg"))
+    
+def setup(bot):
+    bot.add_cog(MemeGen(bot))

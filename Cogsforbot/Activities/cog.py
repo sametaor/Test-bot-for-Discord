8047257138f7e11dcd68c8d5f5367af317f2846d
sametaor <sambit.acharya1@outlook.com>
2022-03-13 21:33:02 +0530

    Added if else for activities with boost lvl 1

--- Cogsforbot/Activities/cog.py
+++ Cogsforbot/Activities/cog.py
@@ -31,27 +31,33 @@ class Activities(commands.Cog):
     async def chess(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.chess)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Chess game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="This is the same old chess that you play on a physical board but in a voice channel!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.chess)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Chess game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="This is the same old chess that you play on a physical board but in a voice channel!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
     
     @activityplay.command()
     async def checkers(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.checker)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Checkers game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="This is the same old checkers(not Chinese checkers, the original one) that you play on a physical board but in a voice channel!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.checker)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Checkers game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="This is the same old checkers(not Chinese checkers, the original one) that you play on a physical board but in a voice channel!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
     
     @activityplay.command()
     async def youtube(self, ctx, channel: nextcord.VoiceChannel = None):
@@ -70,14 +76,17 @@ class Activities(commands.Cog):
     async def ocho(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.ocho)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Ocho game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="If you have played Uno before, this is basically the same game with a different name!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.ocho)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Ocho game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="If you have played Uno before, this is basically the same game with a different name!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
     
     @activityplay.command()
     async def wordsnacks(self, ctx, channel: nextcord.VoiceChannel = None):
@@ -122,53 +131,65 @@ class Activities(commands.Cog):
     async def spellcast(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.spellcast)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Spellcast game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="Play a game of Spellcast with your friends in a Voice Channel!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.spellcast)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Spellcast game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="Play a game of Spellcast with your friends in a Voice Channel!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
 
     @activityplay.command()
     async def awkword(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.awkword)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Awkword game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="Play a game of Awkword with your friends in a Voice Channel!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.awkword)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Awkword game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="Play a game of Awkword with your friends in a Voice Channel!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
 
     @activityplay.command()
     async def lettertile(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.letter_tile)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Letter Tile game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="Play a game of Letter Tile with your friends in a Voice Channel!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.letter_tile)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Letter Tile game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="Play a game of Letter Tile with your friends in a Voice Channel!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
 
     @activityplay.command()
     async def letterleague(self, ctx, channel: nextcord.VoiceChannel = None):
         if channel == None:
             return await ctx.send("Please specify a channel to join/create a game")
-        try:
-            invite_link =  await channel.create_activity_invite(activities.Activity.letter_league)
-        except nextcord.HTTPException:
-            return await ctx.send("Please mention a voice channel to join/create a game")
-        sketchembed = nextcord.Embed(title="Letter League game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
-        sketchembed.add_field(name="How to Play?", value="Play a game of Letter League with your friends in a Voice Channel!")
-        sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
-        await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        if ctx.guild.premium_tier <=1:
+            try:
+                invite_link =  await channel.create_activity_invite(activities.Activity.letter_league)
+            except nextcord.HTTPException:
+                return await ctx.send("Please mention a voice channel to join/create a game")
+            sketchembed = nextcord.Embed(title="Letter League game", description=f"{ctx.author.mention} has created a game in {channel.mention}")
+            sketchembed.add_field(name="How to Play?", value="Play a game of Letter League with your friends in a Voice Channel!")
+            sketchembed.set_thumbnail(url="https://i.imgur.com/KAg60qh.png")
+            await ctx.send(embed=sketchembed, view=MakeLinkBtn(invite_link))
+        elif ctx.guild.premium_tier == 0:
+            await ctx.send("The server needs to be atleast Boosted to Level 1 to play this game/Activity")
 
 def setup(bot):
     bot.add_cog(Activities(bot))

124400a8a39557d49adf14ebc7b20c81f5f7b57d
sametaor <sambit.acharya1@outlook.com>
2022-03-27 20:02:01 +0530

    Removed reaction role function

--- Cogsforbot/Moderation/cog.py
+++ Cogsforbot/Moderation/cog.py
@@ -19,30 +19,6 @@ class Moderation(commands.Cog):
             if file.filename.endswith((".exe", ".dll", ".xlsx")):
                 await msg.delete()
                 await msg.channel.send("No .exe, .dll or .xlsx files allowed!")
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-
-        if payload.member.bot:
-            pass
-
-        else:
-            with open('giverole.json') as react_file:
-                data = json.load(react_file)
-                for x in data:
-                    if x['emoji'] == payload.emoji.name and x['message_id'] == payload.message_id:
-                        role = nextcord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x['role_id'])
-                        await payload.member.add_roles(role)
-    
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, payload):
-
-        with open('giverole.json') as react_file:
-            data = json.load(react_file)
-            for x in data:
-                if x['emoji'] == payload.emoji.name and x['message_id'] == payload.message_id:
-                    role = nextcord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x['role_id'])
-
-                    await self.bot.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
 
     @commands.command(description="Toggles commands to be enabled or disabled")
     @commands.has_guild_permissions(ban_members=True)
@@ -293,34 +269,6 @@ class Moderation(commands.Cog):
         }
         channel = await ctx.guild.create_text_channel(name=name, overwrites=overwrites)
         await ctx.send(f"New channel {channel.name} has been created!")
-    
-    @commands.command(pass_context=True)
-    @commands.has_permissions(manage_roles=True)
-    async def giverole(self, ctx, emoji, role : nextcord.Role, *, rolemessage):
-        emb = nextcord.Embed(title='Reaction role!', description=rolemessage, colour = role.colour)
-        emb.set_footer(text="This reaction embed will expire after 30 days! ")
-        rolemsg = await ctx.channel.send(embed=emb)
-        await rolemsg.add_reaction(emoji)
-
-        with open('giverole.json') as json_file:
-            data = json.load(json_file)
-
-            new_react_role = {'role_name' : role.name,
-                'role_id' : role.id,
-                'emoji' : emoji,
-                'message_id' : rolemsg.id}
-
-            data.append(new_react_role)
-
-        with open('giverole.json','w') as f:
-            json.dump(data,f,indent=4)
-        await asyncio.sleep(2592000)
-        embvone = nextcord.Embed(title='Reactions role!', description=rolemessage, colour = nextcord.Colour.magenta())
-        embvone.set_footer(text="This reaction embed has expired, ask the admin to make a new one!")
-        await rolemsg.edit(embed=embvone)
-        with open('giverole.json', 'w') as f:
-            data.remove(new_react_role)
-            json.dump(data,f,indent=4)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))

2ed4ca9003716a8c4a6a8977a6e953e43a507f8d
sametaor <sambit.acharya1@outlook.com>
2023-02-16 21:14:17 +0530

    Ai restart

--- Cogsforbot/Miscellaneous/cog.py
+++ Cogsforbot/Miscellaneous/cog.py
@@ -1,9 +1,10 @@
 import nextcord
 from nextcord.ext import commands
-from prsaw import RandomStuffV4
 import requests
 import os
 from dotenv import load_dotenv
+
+load_dotenv('aichat.env')
 
 class AIresponse(commands.Cog):
     def __init__(self, bot):
@@ -15,12 +16,23 @@ class AIresponse(commands.Cog):
         if testbot.user == msg.author:
             return
         if msg.channel.name == 'ai-chat':
+            url = "https://random-stuff-api.p.rapidapi.com/ai/response"
             text = msg.content
-            rs = RandomStuffV4()
-            response = rs.get_ai_response(text)
-            print(response)
-            await msg.reply(str(response))
-            rs.close()
+            querystring = {"message":text, "user_id":"420"}
+
+            headers = {
+            'authorization': os.getenv('RANDOMSTUFFAPIKEY'),
+            'x-rapidapi-host': "random-stuff-api.p.rapidapi.com",
+            'x-rapidapi-key': os.getenv('RAPIDAPIKEY')
+            }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+
+            r=response.text
+            r=r.split('"message":"')
+            r=r[1]
+            r=r.replace('"}', '')
+            print(r)
+            await msg.reply(str(r))
         await testbot.process_commands(msg)
     
     @commands.command()
